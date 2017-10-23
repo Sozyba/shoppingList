5a51cb9e305333255cdc70504c2136f723c03c8f
user <sozy.31.1.2000@hotmail.com>
2017-10-23 09:40:21 +0300

    12

--- app/src/main/java/com/badran/suzan/shoppinglist/Login.java
+++ app/src/main/java/com/badran/suzan/shoppinglist/Login.java
@@ -1,18 +1,27 @@
 package com.badran.suzan.shoppinglist;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
     private Button btnIn,btnUp;
     private EditText etEmail;
     private EditText etPassword;
     private Button btnLog;
-
+    private FirebaseAuth auth;
+    private FirebaseUser firebaseUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,5 +38,26 @@ public class Login extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        auth = FirebaseAuth.getInstance();
+        firebaseUser = auth.getCurrentUser();
+    }
+    private void dataHandler()
+    private  void signIn(String email, String Password){
+        auth.signInWithEmailAndPassword(email, Password).addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(Login.this, "singIn Successful.", Toast.LENGTH_SHORT).show();
+                    finish();
+                } else {
+                    Toast.makeText(Login.this, "singIn failed." + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    task.getException().printStackTrace();
+                }
+            }
+
+
+
+        });
+
     }
 }
--- app/src/main/java/com/badran/suzan/shoppinglist/mainlistfragments/signup.java
+++ app/src/main/java/com/badran/suzan/shoppinglist/mainlistfragments/signup.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.badran.suzan.shoppinglist.Login;
 import com.badran.suzan.shoppinglist.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -41,59 +42,44 @@ public class signup extends AppCompatActivity {
     }
 
 
+    private void dataHandler() {
+        String stEmail = Email.getText().toString();
+        String stName = Name.getText().toString();
+        String stPassword = Password.getText().toString();
+        String stRepassword = Repassword.getText().toString();
+        boolean isOk = true;
+        if ((stEmail.length() == 0) || stEmail.indexOf('@') < 1) {
+            Email.setError("wrong email");
+            isOk = false;
 
-private void dataHandler()
-{
-    String stEmail=Email.getText().toString();
-    String stName=Name.getText().toString();
-    String stPassword=Password.getText().toString();
-    String stRepassword=Repassword.getText().toString();
-    boolean isOk=true;
-    if( (stEmail.length()==0) || stEmail.indexOf('@')<1)
-    {
-        Email.setError("wrong email");
-        isOk=false;
-
-    }
-    if ((stPassword.length()<8) || stPassword.equals(stRepassword)==false)
-    {
-        Password.setError("Bad Password");
-        isOk=false;
-
-    }
-    if (isOk)
-    creatAcount(stEmail,stPassword);
-
-
-}
-
-
-
-private void creatAcount(String Email,String Password){
-auth.createUserWithEmailAndPassword(Email,Password).addOnCompleteListener(signup.this, new OnCompleteListener<AuthResult>() {
-    @Override
-    public void onComplete(@NonNull Task<AuthResult> task) {
-        if (task.isSuccessful()) {
-            Toast.makeText(signup.this, "Authentication Successful", Toast.LENGTH_SHORT).show();
-            finish();
-        } else {
-            Toast.makeText(signup.this, "Authentication failed." + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-            task.getException().printStackTrace();
         }
+        if ((stPassword.length() < 8) || stPassword.equals(stRepassword) == false) {
+            Password.setError("Bad Password");
+            isOk = false;
+
+        }
+        if (isOk)
+            creatAcount(stEmail, stPassword);
+
+
+    }
+
+    private void creatAcount(String Email, String Password) {
+        auth.createUserWithEmailAndPassword(Email, Password).addOnCompleteListener(signup.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(signup.this, "Authentication Successful", Toast.LENGTH_SHORT).show();
+                    finish();
+                } else {
+                    Toast.makeText(signup.this, "Authentication failed." + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    task.getException().printStackTrace();
+                }
+            }
+
+
+        });
     }
 
 
-});
-}
-FirebaseUser user=FirebaseAuth.getInstance().getCurrentUser();
-if(user!= null){
-        //User is signed in
-}else{
-        //No user is signed in
-
-    }
-private  void updateUserProfile(FirebaseUser user)
-{
-UserProfileChangeRequest.Builder builder=new UserProfileChangeRequest.Builder();
-  builder.setDisplayName()
 }
