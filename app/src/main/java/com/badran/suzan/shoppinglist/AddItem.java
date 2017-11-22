70491a098c4a65ddc102fcf59c426f0472cd9fd2
user <sozy.31.1.2000@hotmail.com>
2017-11-22 10:51:37 +0200

    12

--- app/src/main/java/com/badran/suzan/shoppinglist/AddItem.java
+++ app/src/main/java/com/badran/suzan/shoppinglist/AddItem.java
@@ -1,12 +1,17 @@
 package com.badran.suzan.shoppinglist;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -32,15 +37,56 @@ public class AddItem extends AppCompatActivity {
 
     }
 
-    public void dataHander() {
+    public void dataHander()
+            //1.get data from the fields
+    {
         String stname = etName.getText().toString();
         String stprice = etPrice.getText().toString();
         String stamount = etAmount.getText().toString();
         String stunits = etUnits.getText().toString();
+        //2. todo Validate fields input
+        //isok=true;...
+
+        //3.data manipulation
         double amount = Double.parseDouble(stamount);
         double price = Double.parseDouble(stprice);
+
+        //4.building data objects
+        //Product p=new Product();
+        //p.setName(stname);
+        //p.setAmount(amount);
+        //p.setPrice(price);
+        //p.setCompleted(false);
+
+
+        //5.to get user emails...user info
+        //to get user emaill...user info
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
+        String email = user.getEmail();
+        email = email.replace('.', '*');
+
+
+        //6.bulding data refernce = data path= ata address
         DatabaseReference reference;
         reference = FirebaseDatabase.getInstance().getReference();
+
+        //7
+        reference.child(email).child("myList").push().setValue(p).
+                addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(AddItem.this, "Add Product Successful", Toast.LENGTH_SHORT);
+                        } else {
+                            Toast.makeText(AddItem.this, "Add Product Failed", Toast.LENGTH_SHORT);
+                        }
+                    }
+                });
+
+
+
+        //todo Testing
         reference.child("List").setPriority("soso");
     }
 }
---
+++ app/src/main/java/com/badran/suzan/shoppinglist/data/Product.java
@@ -0,0 +1,9 @@
+package com.badran.suzan.shoppinglist.data;
+
+/**
+ * Created by user on 22/11/2017.
+ */
+
+public class Product {
+    
+}
